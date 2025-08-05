7a041f12120f8788ec32add1fa7b29a218fee42b
Himanshu Dwivedi <himanshudwivedi1224@gmail.com>
2025-08-05 08:58:46 +0530

    added back function

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom'; // Import Link and useLocation
 import './Header.css';
 import { FaSun, FaMoon } from 'react-icons/fa6'; // Import icons
 
@@ -8,6 +9,9 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ toggleTheme, currentTheme }) => {
+  const location = useLocation();
+  const isBlogPostPage = location.pathname.startsWith('/MyPortfolio/blog/');
+
   return (
     <header className="header">
       <div className="container">
@@ -16,13 +20,27 @@ const Header: React.FC<HeaderProps> = ({ toggleTheme, currentTheme }) => {
           <p>Full Stack Developer</p>
         </div>
         <nav>
-          <a href="#about">About</a>
-          <a href="#experience">Experience</a>
-          <a href="#projects">Projects</a>
-          <a href="#blog">Blog</a> {/* Added Blog link */}
-          <a href="#education">Education</a>
-          <a href="#skills">Skills</a>
-          <a href="#contact">Contact</a>
+          {isBlogPostPage ? (
+            <>
+              <Link to="/MyPortfolio">About</Link>
+              <Link to="/MyPortfolio">Experience</Link>
+              <Link to="/MyPortfolio">Projects</Link>
+              <Link to="/MyPortfolio">Blog</Link>
+              <Link to="/MyPortfolio">Education</Link>
+              <Link to="/MyPortfolio">Skills</Link>
+              <Link to="/MyPortfolio">Contact</Link>
+            </>
+          ) : (
+            <>
+              <a href="#about">About</a>
+              <a href="#experience">Experience</a>
+              <a href="#projects">Projects</a>
+              <a href="#blog">Blog</a>
+              <a href="#education">Education</a>
+              <a href="#skills">Skills</a>
+              <a href="#contact">Contact</a>
+            </>
+          )}
           <button onClick={toggleTheme} className="theme-toggle-button">
             {currentTheme === 'dark' ? <FaSun /> : <FaMoon />}
           </button>
